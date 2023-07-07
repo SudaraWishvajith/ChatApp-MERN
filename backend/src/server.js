50e4f938783f3d9b2deafd37acc031f6b12796cc
SudaraWishvajith <it21221378@my.sliit.lk>
2023-07-07 13:10:33 +0530

    update main.jsx
    remove unnecessary things

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const axios = require('axios');
+const env = require('dotenv');
 
 app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async(req,res) =>{
     const { userName } = req.body;
-    return res.json({
-        userName: userName, 
-        secret: ""
-    });
+
+    try{
+        const r = await axios.put(
+            "https://api.chatengine.io/users/",
+            { username: userName, secret: userName, first_name: userName },
+            { headers: { "Private-Key": `${process.env.SECRET}`}}  
+        );
+        return res.status(r.status).json(r.data);
+    }catch(e){
+        return res.status(e.response.status).json(e.response.data);
+    }
 });
 
 app.listen(3001,() => {
